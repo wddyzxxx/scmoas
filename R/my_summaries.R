b4d50c468cc34db6bbf57d49ec64f3b2f6fa846b
wddyzxxx <126195765@qq.com>
2024-03-18 14:28:58 +0800

    Initial commit

---
+++ R/my_summaries.R
@@ -0,0 +1,26 @@
+# This function creates a summary for a numeric vector
+numeric_summary <- function(x, na.rm=FALSE){
+
+  min = min(x, na.rm=na.rm)
+  max = max(x, na.rm=na.rm)
+  mean = mean(x, na.rm=na.rm)
+  sd = sd(x, na.rm=na.rm)
+  length = length(x)
+  Nmiss = sum(is.na(x))
+
+  c(min=min, max=max, mean=mean, sd=sd, length=length, Nmiss=Nmiss)
+
+}
+
+# This function creates a summary for a character vector
+char_summary <- function(x, na.rm=FALSE){
+
+  length = length(x)
+  Nmiss = sum(is.na(x))
+  Nunique = length(unique(x))
+
+  c(length = length,
+    Nmiss = Nmiss,
+    Nunique = Nunique )
+
+}
